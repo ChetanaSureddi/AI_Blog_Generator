2b841a7540387dd44a3b9e50c3a067c7dfc834bd
chetana <chetana6006@gmail.com>
2025-07-21 16:22:58 -0500

    Rest of the changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
 
 const App = () => {
   return (
     <div>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/blog/:id" element={<Blog />}>
-      </Route>
+      <Route path="/blog/:id" element={<Blog />}/>
+      <Route Path="/admin" element={<Layout/>}/>
+        <Route index element={<Dashboard/>}/>
+        <Route path='addBlog' element={<AddBlog/>} />
+        <Route path='listBlog' element={<ListBlog/>} />
+        <Route path='Comments' element={<Comments/>} />
     </Routes>
     </div>
   )
