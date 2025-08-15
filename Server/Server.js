c98cd9541edfcb56423975448e4ffa172165eed9
chetana <chetana6006@gmail.com>
2025-08-15 16:02:40 -0500

    Server side changes

--- Server/Server.js
+++ Server/Server.js
@@ -25,8 +25,7 @@ app.use('/api/blog', blogRouter)
 
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, ()=>{
-//     console.log('server is running on port ' + PORT)
-// })
+app.listen(PORT, ()=>{
+    console.log('server is running on port ' + PORT)
+})
 
-export default app; 
